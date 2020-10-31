8b37230d7cd42aa2aa8e4063e8e402a542c67f1a
Petr Pondělík <pondepe1@fit.cvut.cz>
2020-10-31 19:06:08 +0100

    HW2: FPTAS times aggregated

--- homework/hw2/src/fptas_times.py
+++ homework/hw2/src/fptas_times.py
@@ -3,12 +3,37 @@ basePath = './../results/measurement'
 # Array of data-sets
 datasetsArr: list = ['NK', 'ZKC', 'ZKW']
 
+instancesCntMapping: dict = {
+    'NK': {
+        4: 1500,
+        10: 1500,
+        15: 1500,
+        20: 1500,
+        22: 1500
+    },
+    'ZKC': {
+        4: 1500,
+        10: 1500,
+        15: 1500,
+        20: 1500,
+        22: 1500
+    },
+    'ZKW': {
+        4: 2862,
+        10: 888,
+        15: 462,
+        20: 231,
+        22: 237
+    }
+}
+
 def aggregate(dataset: str):
     sourceFile = open('{}/{}/{}/time.txt'.format(basePath, dataset, 'FPTAS'), 'r')
     outputFile = open('{}/{}/{}/time_agg.txt'.format(basePath, dataset, 'FPTAS'), 'w+')
 
     lines: list = sourceFile.readlines()
 
+    itemsCnt: int = 0
     eps: float = 0.0
     measurementRun: int = 0
     timeAcc: float = 0.0
@@ -18,9 +43,17 @@ def aggregate(dataset: str):
 
     for line in lines:
         lineList: list = line.strip().split(' ')
-        lineEps: float = float(lineList[0])
-        lineMeasurementRun: int = int(lineList[1])
-        lineTime: float = float(lineList[2]) * 1000
+        lineItemsCnt: int = int(lineList[0])
+        lineEps: float = float(lineList[1])
+        lineMeasurementRun: int = int(lineList[2])
+        lineTime: float = float(lineList[3]) * 1000
+
+        if lineItemsCnt != itemsCnt:
+            itemsCnt = lineItemsCnt
+            eps = lineEps
+            measurementRun = 0
+            timeAcc, cnt = 0.0, 0
+            timeAvg, timeMax = 0.0, 0.0
 
         if lineEps != eps:
             eps = lineEps
@@ -33,9 +66,9 @@ def aggregate(dataset: str):
         if lineTime > timeMax:
             timeMax = lineTime
 
-        if cnt == 1500:
+        if cnt == instancesCntMapping[dataset][itemsCnt]:
             timeAvg = timeAcc/cnt
-            outputFile.write('{} {} {}\n'.format(eps, timeAvg, timeMax))
+            outputFile.write('{}\t{}\t{}\t{}\n'.format(itemsCnt, eps, timeAvg, timeMax))
 
         if lineMeasurementRun != measurementRun:
             measurementRun = lineMeasurementRun
