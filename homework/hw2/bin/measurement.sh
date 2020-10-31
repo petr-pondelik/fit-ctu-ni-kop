fefe7e079049a48001dd1863de256992878ab32f
Petr Pondělík <pondepe1@fit.cvut.cz>
2020-10-31 01:14:41 +0100

    HW2: ZKW times measured.

--- homework/hw2/bin/measurement.sh
+++ homework/hw2/bin/measurement.sh
@@ -7,14 +7,55 @@ resultPath='./../results/measurement'
 ##  Measure algorithms times  ##
 ################################
 
+## Array of data-sets
+#datasetsArr=( NK ZKC )
+#
+## Array of algorithms
+#algorithmsArr=( BruteForce BranchAndBound GreedyRedux DynamicProgramming )
+#
+## Array of items number
+#itemsCntArr=( 4 10 15 20 22 )
+#
+## Prepare result files
+#for dataset in "${datasetsArr[@]}"
+#do
+#  for algorithm in "${algorithmsArr[@]}"
+#  do
+#    rm "${resultPath}/${dataset}/${algorithm}/time.txt"
+#    touch "${resultPath}/${dataset}/${algorithm}/time.txt"
+#  done
+#done
+#
+#for dataset in "${datasetsArr[@]}"
+#do
+#  for algorithm in "${algorithmsArr[@]}"
+#  do
+#    for n in "${itemsCntArr[@]}"
+#    do
+#      # Write result into measurement result file
+##      echo "Items amount: ${n}" >> "${resultPath}/${dataset}/${algorithm}/time.txt"
+#      for measurementRun in {1..3}
+#        do
+#        for instanceInx in {0..499}
+#        do
+#          python3 ./../src/main.py "${algorithm}" "${dataset}" "${n}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
+#        done
+#      done
+#    done
+#  done
+#done
+
 # Array of data-sets
-datasetsArr=( NK ZKC ZKW )
+datasetsArr=( ZKW )
 
 # Array of algorithms
 algorithmsArr=( BruteForce BranchAndBound GreedyRedux DynamicProgramming )
 
 # Array of items number
 itemsCntArr=( 4 10 15 20 22 )
+
+# Array of indexes cnt
+indexesCntArr=( 953 295 153 76 78 )
 
 # Prepare result files
 for dataset in "${datasetsArr[@]}"
@@ -30,15 +71,15 @@ for dataset in "${datasetsArr[@]}"
 do
   for algorithm in "${algorithmsArr[@]}"
   do
-    for n in "${itemsCntArr[@]}"
+    for i in "${!itemsCntArr[@]}"
     do
       # Write result into measurement result file
 #      echo "Items amount: ${n}" >> "${resultPath}/${dataset}/${algorithm}/time.txt"
       for measurementRun in {1..3}
         do
-        for instanceInx in {0..499}
+        for instanceInx in $(seq 0 "${indexesCntArr[i]}")
         do
-          python3 ./../src/main.py "${algorithm}" "${dataset}" "${n}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
+          python3 ./../src/main.py "${algorithm}" "${dataset}" "${itemsCntArr[i]}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
         done
       done
     done
