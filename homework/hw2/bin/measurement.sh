d3bb5dc69e2034b215e0a5db797eb4ef3f48ce17
Petr Pondělík <pondepe1@fit.cvut.cz>
2020-10-31 16:18:17 +0100

    HW2: Greedy and GreedyRedux relative errors measured.

--- homework/hw2/bin/measurement.sh
+++ homework/hw2/bin/measurement.sh
@@ -45,70 +45,70 @@ resultPath='./../results/measurement'
 #  done
 #done
 
-# Array of data-sets
-datasetsArr=( ZKW )
-
-# Array of algorithms
-algorithmsArr=( BruteForce BranchAndBound GreedyRedux DynamicProgramming )
-
-# Array of items number
-itemsCntArr=( 4 10 15 20 22 )
-
-# Array of indexes cnt
-indexesCntArr=( 953 295 153 76 78 )
-
-# Prepare result files
-for dataset in "${datasetsArr[@]}"
-do
-  for algorithm in "${algorithmsArr[@]}"
-  do
-    rm "${resultPath}/${dataset}/${algorithm}/time.txt"
-    touch "${resultPath}/${dataset}/${algorithm}/time.txt"
-  done
-done
-
-for dataset in "${datasetsArr[@]}"
-do
-  for algorithm in "${algorithmsArr[@]}"
-  do
-    for i in "${!itemsCntArr[@]}"
-    do
-      # Write result into measurement result file
-#      echo "Items amount: ${n}" >> "${resultPath}/${dataset}/${algorithm}/time.txt"
-      for measurementRun in {1..3}
-        do
-        for instanceInx in $(seq 0 "${indexesCntArr[i]}")
-        do
-          python3 ./../src/main.py "${algorithm}" "${dataset}" "${itemsCntArr[i]}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
-        done
-      done
-    done
-  done
-done
+## Array of data-sets
+#datasetsArr=( ZKW )
+#
+## Array of algorithms
+#algorithmsArr=( BruteForce BranchAndBound GreedyRedux DynamicProgramming )
+#
+## Array of items number
+#itemsCntArr=( 4 10 15 20 22 )
+#
+## Array of indexes cnt
+#indexesCntArr=( 953 295 153 76 78 )
+#
+## Prepare result files
+#for dataset in "${datasetsArr[@]}"
+#do
+#  for algorithm in "${algorithmsArr[@]}"
+#  do
+#    rm "${resultPath}/${dataset}/${algorithm}/time.txt"
+#    touch "${resultPath}/${dataset}/${algorithm}/time.txt"
+#  done
+#done
+#
+#for dataset in "${datasetsArr[@]}"
+#do
+#  for algorithm in "${algorithmsArr[@]}"
+#  do
+#    for i in "${!itemsCntArr[@]}"
+#    do
+#      # Write result into measurement result file
+##      echo "Items amount: ${n}" >> "${resultPath}/${dataset}/${algorithm}/time.txt"
+#      for measurementRun in {1..3}
+#        do
+#        for instanceInx in $(seq 0 "${indexesCntArr[i]}")
+#        do
+#          python3 ./../src/main.py "${algorithm}" "${dataset}" "${itemsCntArr[i]}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
+#        done
+#      done
+#    done
+#  done
+#done
 
 #################################
 ##  Measure greedy heuristics  ##
 #################################
 
-### Array of data-sets
-#greedyDatasetsArr=( NK ZKC ZKW )
-#
-### Array of algorithms
-#greedyAlgorithmsArr=( Greedy GreedyRedux )
-#
-### Array of items number
-#greedyItemsCntArr=( 4 10 15 20 22)
-#
-#for dataset in "${greedyDatasetsArr[@]}"
-#do
-#  for algorithm in "${greedyAlgorithmsArr[@]}"
-#  do
-#    for n in "${greedyItemsCntArr[@]}"
-#    do
-#      python3 ./../src/main.py "${algorithm}" "${dataset}" "${n}" 1 0-499 1
-#    done
-#  done
-#done
+## Array of data-sets
+greedyDatasetsArr=( NK ZKC ZKW )
+
+## Array of algorithms
+greedyAlgorithmsArr=( Greedy GreedyRedux )
+
+## Array of items number
+greedyItemsCntArr=( 4 10 15 20 22)
+
+for dataset in "${greedyDatasetsArr[@]}"
+do
+  for algorithm in "${greedyAlgorithmsArr[@]}"
+  do
+    for n in "${greedyItemsCntArr[@]}"
+    do
+      python3 ./../src/main.py "${algorithm}" "${dataset}" "${n}" 1 0-499 1
+    done
+  done
+done
 
 ###############################
 ##  Measure FPTAS algorithm  ##
