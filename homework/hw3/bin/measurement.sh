a343adc3cad88bb31d8deb0022790d3759604e75
Petr Pondělík <pondepe1@fit.cvut.cz>
2020-11-12 22:42:19 +0100

    HW3: B&B times measured.

--- homework/hw3/bin/measurement.sh
+++ homework/hw3/bin/measurement.sh
@@ -10,38 +10,38 @@ itemsCntArr=( 5 10 15 20 22 25 )
 ##      Measure Branch & Bound times
 ###################################################
 
-# Array of data-sets
-datasetsArr=( m005 m01 m02 m03 )
-
-# Array of algorithms
-algorithmsArr=( BranchAndBound )
-
-# Prepare result files
-for dataset in "${datasetsArr[@]}"
-do
-  for algorithm in "${algorithmsArr[@]}"
-  do
-    rm "${resultPath}/${algorithm}/time.txt"
-    touch "${resultPath}/${algorithm}/time.txt"
-  done
-done
-
-for dataset in "${datasetsArr[@]}"
-do
-  for algorithm in "${algorithmsArr[@]}"
-  do
-    for n in "${itemsCntArr[@]}"
-    do
-      for measurementRun in {1..3}
-        do
-        for instanceInx in {0..499}
-        do
-          python3 ./../src/main.py "${algorithm}" "${dataset}" "${n}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
-        done
-      done
-    done
-  done
-done
+## Array of data-sets
+#datasetsArr=( m005 m01 m02 m03 )
+#
+## Array of algorithms
+#algorithmsArr=( BranchAndBound )
+#
+## Prepare result files
+#for dataset in "${datasetsArr[@]}"
+#do
+#  for algorithm in "${algorithmsArr[@]}"
+#  do
+#    rm "${resultPath}/${algorithm}/time.txt"
+#    touch "${resultPath}/${algorithm}/time.txt"
+#  done
+#done
+#
+#for dataset in "${datasetsArr[@]}"
+#do
+#  for algorithm in "${algorithmsArr[@]}"
+#  do
+#    for n in "${itemsCntArr[@]}"
+#    do
+#      for measurementRun in {1..3}
+#        do
+#        for instanceInx in {0..499}
+#        do
+#          python3 ./../src/main.py "${algorithm}" "${dataset}" "${n}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
+#        done
+#      done
+#    done
+#  done
+#done
 
 ###################################################
 ##      Measure Dynamic Programming times
