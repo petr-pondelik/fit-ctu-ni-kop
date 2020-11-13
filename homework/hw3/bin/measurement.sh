845e1f710c87375ca5795c4098f4aaa7850b3b24
Petr Pondělík <pondepe1@fit.cvut.cz>
2020-11-13 13:48:53 +0100

    HW3: DP measurement finished.

--- homework/hw3/bin/measurement.sh
+++ homework/hw3/bin/measurement.sh
@@ -10,58 +10,21 @@ itemsCntArr=( 5 10 15 20 22 25 )
 ##      Measure Branch & Bound times
 ###################################################
 
-mItemsCntArr=( 5 10 15 20 22 )
-
-# Array of data-sets
-datasetsArr=( m01 m02 m03 m04 m05 m06 m07 m08 m09 )
-
-# Array of algorithms
-algorithmsArr=( BranchAndBound )
-
-# Prepare result files
-for dataset in "${datasetsArr[@]}"
-do
-  for algorithm in "${algorithmsArr[@]}"
-  do
-    rm "${resultPath}/${algorithm}/time.txt"
-    touch "${resultPath}/${algorithm}/time.txt"
-  done
-done
-
-for dataset in "${datasetsArr[@]}"
-do
-  for algorithm in "${algorithmsArr[@]}"
-  do
-    for n in "${mItemsCntArr[@]}"
-    do
-      for measurementRun in {1..3}
-        do
-        for instanceInx in {0..499}
-        do
-          python3 ./../src/main.py "${algorithm}" "${dataset}" "${n}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
-        done
-      done
-    done
-  done
-done
-
-###################################################
-##      Measure Dynamic Programming times
-###################################################
-
+#mItemsCntArr=( 5 10 15 20 22 )
+#
 ## Array of data-sets
-#datasetsArr=( C100 C500 C1500 C4000 )
+#datasetsArr=( m01 m02 m03 m04 m05 m06 m07 m08 m09 )
 #
 ## Array of algorithms
-#algorithmsArr=( DynamicProgramming )
+#algorithmsArr=( BranchAndBound )
 #
 ## Prepare result files
 #for dataset in "${datasetsArr[@]}"
 #do
 #  for algorithm in "${algorithmsArr[@]}"
 #  do
-#    rm "${resultPath}/${dataset}/${algorithm}/time.txt"
-#    touch "${resultPath}/${dataset}/${algorithm}/time.txt"
+#    rm "${resultPath}/${algorithm}/time.txt"
+#    touch "${resultPath}/${algorithm}/time.txt"
 #  done
 #done
 #
@@ -69,16 +32,53 @@ done
 #do
 #  for algorithm in "${algorithmsArr[@]}"
 #  do
-#    for n in "${itemsCntArr[@]}"
+#    for n in "${mItemsCntArr[@]}"
 #    do
 #      for measurementRun in {1..3}
 #        do
-#          for instanceInx in {0..499}
-#          do
-#            python3 ./../src/main.py "${algorithm}" "${dataset}" "${n}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
-#          done
+#        for instanceInx in {0..499}
+#        do
+#          python3 ./../src/main.py "${algorithm}" "${dataset}" "${n}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
 #        done
 #      done
 #    done
 #  done
 #done
+
+###################################################
+##      Measure Dynamic Programming times
+###################################################
+
+# Array of data-sets
+datasetsArr=( C1500 C4000 )
+
+# Array of algorithms
+algorithmsArr=( DynamicProgramming )
+
+# Prepare result files
+for dataset in "${datasetsArr[@]}"
+do
+  for algorithm in "${algorithmsArr[@]}"
+  do
+    rm "${resultPath}/${dataset}/${algorithm}/time.txt"
+    touch "${resultPath}/${dataset}/${algorithm}/time.txt"
+  done
+done
+
+for dataset in "${datasetsArr[@]}"
+do
+  for algorithm in "${algorithmsArr[@]}"
+  do
+    for n in "${itemsCntArr[@]}"
+    do
+      for measurementRun in {1..3}
+        do
+          for instanceInx in {0..499}
+          do
+            python3 ./../src/main.py "${algorithm}" "${dataset}" "${n}" 0 "${instanceInx}"-"${instanceInx}" 1 | head -n 2 | sed 's/.*in\s//' | sed 's/\sseconds//' | awk -v mr="$measurementRun" 'NR%2{printf "%s %s ",$0,mr;next;}1' >> "${resultPath}/${dataset}/${algorithm}/time.txt"
+          done
+        done
+      done
+    done
+  done
+done
